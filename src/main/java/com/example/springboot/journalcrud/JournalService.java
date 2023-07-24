038828360bc9953c9053cba6b2baf6be76b96658
benterkidhiaeddine <benterki.dhiaeddine@gmail.com>
2023-07-24 18:51:19 +0100

    Delete unnecessary Request body annotation

--- src/main/java/com/example/springboot/journalcrud/JournalService.java
+++ src/main/java/com/example/springboot/journalcrud/JournalService.java
@@ -5,7 +5,6 @@ import com.example.springboot.journal.Journal;
 import com.example.springboot.journal.JournalRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @Service
 //The required args constructor is for final fields so they can get injected automaticaly by the spring application
@@ -14,7 +13,7 @@ public class JournalService {
 
     private final JournalRepository journalRepository;
 
-    public MessageResponse addJournal(@RequestBody JournalRequest request){
+    public MessageResponse addJournal( JournalRequest request){
         var journal = Journal.builder()
                 .description(request.getDescription())
                 .build();
