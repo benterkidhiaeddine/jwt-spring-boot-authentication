15514e42041df9ddfa629829872eb7af58a37a8a
benterkidhiaeddine <benterki.dhiaeddine@gmail.com>
2023-07-24 18:34:13 +0100

    Add @Data annotation to ApiError class

--- src/main/java/com/example/springboot/exceptions/ApiError.java
+++ src/main/java/com/example/springboot/exceptions/ApiError.java
@@ -2,12 +2,13 @@ package com.example.springboot.exceptions;
 
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
 import org.springframework.http.HttpStatus;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
-
+@Data
 class ApiError {
     private HttpStatus status;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
