7e5e8ea1984ac662021eb2c287395c58734a5b6f
benterkidhiaeddine <benterki.dhiaeddine@gmail.com>
2023-07-24 18:34:55 +0100

    Used better naming convention for journals endpoints

--- src/main/java/com/example/springboot/journalcrud/JournalController.java
+++ src/main/java/com/example/springboot/journalcrud/JournalController.java
@@ -1,20 +1,28 @@
 package com.example.springboot.journalcrud;
 
 
+import com.example.springboot.journal.Journal;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/journals")
+@RequestMapping("/api/v1")
 public class JournalController {
 
 
     private final JournalService journalService;
-    @PostMapping("")
+    @PostMapping("/journals")
     public ResponseEntity<MessageResponse> postJournal(@RequestBody JournalRequest request){
         return ResponseEntity.ok(journalService.addJournal(request));
+    }
+
+    @GetMapping("/journals/{JournalId}")
+    public ResponseEntity<Journal> getJournal(@PathVariable("JournalId") Integer id){
+
+        var jounral = journalService.getJournalById(id);
+        return ResponseEntity.ok(jounral);
     }
 
 
